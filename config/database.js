1a7d3d20c490aaa28d2566cf9952d444ffbe2851
Gokulnath R. <gokulnath.r@owcdomain.com>
2025-08-12 12:12:27 +0530

    add anew chages in database connection

--- config/database.js
+++ config/database.js
@@ -5,10 +5,7 @@ dotenv.config();
 export async function connectDB() {
     try {
         console.log("🚀 ~ connectDB ~ process.env.MONGODB_URI:", process.env.MONGODB_URI)
-        const conn = await mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost:27017/fullstack-oauth-app', {
-            useNewUrlParser: true,
-            useUnifiedTopology: true,
-        });
+        const conn = await mongoose.connect(process.env.MONGODB_URI);;
         console.log(`MongoDB Connected: ${conn.connection.host}`);
     } catch (error) {
         console.error('Database connection error:', error.message);
