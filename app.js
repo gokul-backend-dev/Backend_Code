c4a76ef123db9f4b9c9ee4452ddf58a53bafca6a
Gokulnath R. <gokulnath.r@owcdomain.com>
2025-08-12 13:00:47 +0530

    Modified listen app.js

--- app.js
+++ app.js
@@ -70,6 +70,6 @@ app.get('/api/get', (req, res) => {
 // app.use('/api/tickets', ticketRoutes);
 
 
-app.listen(PORT, () => {
-    console.log(`🚀 Server running on http://localhost:${PORT}`);
+app.listen(PORT, '0.0.0.0', () => {
+    console.log(`🚀 Server running on http://0.0.0.0:${PORT}`);
 });
